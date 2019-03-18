06c09e625b1119f91c71edab442a9fe0aef39853
THELAZYKING <bhaskar3797mishra@gmail.com>
2019-03-18 21:36:41 +0530

    White Board working

--- public/front-client.js
+++ public/front-client.js
@@ -15,3 +15,41 @@
     // set canvas to full browser width/height
     canvas.width = width;
     canvas.height = height;
+
+
+    // register mouse event handlers
+    canvas.onmousedown = function(e){ mouse.click = true; };
+    canvas.onmouseup = function(e){ mouse.click = false; };
+
+    canvas.onmousemove = function(e) {
+        // normalize mouse position to range 0.0 - 1.0
+        mouse.pos.x = e.clientX / width;
+        mouse.pos.y = e.clientY / height;
+        mouse.move = true;
+    };
+
+    // draw line received from server
+    socket.on('draw_line', function (data) {
+        var line = data.line;
+        context.beginPath();
+        context.moveTo(line[0].x * width, line[0].y * height);
+        context.lineTo(line[1].x * width, line[1].y * height);
+        context.stroke();
+    });
+
+// main loop, running every 25ms
+    /*
+    @rishabh - settimeout loops
+     */
+    function mainLoop() {
+        // check if the user is drawing
+        if (mouse.click && mouse.move && mouse.pos_prev) {
+            // send line to to the server
+            socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
+            mouse.move = false;
+        }
+        mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+        setTimeout(mainLoop, 25);
+    }
+    mainLoop();
+});
