abc8d76acf0b2731531d0e47a8fb551005c4d967
THELAZYKING <bhaskar3797mishra@gmail.com>
2019-03-18 19:47:22 +0530

    Server set-up and Working

---
+++ script.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    socketIo = require('socket.io');
+
+// start webserver on port 8080
+var server =  http.createServer(app);
+var io = socketIo.listen(server);
+server.listen(8080);
+// add directory with our static files
+app.use(express.static(__dirname + '/public'));
+console.log("Server running on 127.0.0.1:8080");
