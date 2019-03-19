df78d5cb0b0b1684f2fc6510df0b31c423a45f2d
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-03-19 10:38:35 +0530

    update server port

--- server/index.js
+++ server/index.js
@@ -1,15 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
-const port = 6060;
-app.use(express.static(path.join(__dirname, "../build")));
+const port = 3030;
+app.use(express.static(path.join(__dirname, '../build')));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 // eslint-disable-next-line no-console
-app.listen(port, () =>
-  console.log(`Express server is running on http://localhost:${port}/`)
-);
+app.listen(port, () => console.log(`Express server is running on http://localhost:${port}/`));
