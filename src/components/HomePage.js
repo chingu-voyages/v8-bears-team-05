5e9b7ffc535de0e8c38e74eb61cb974e498ffdf4
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-03-24 15:01:46 +0530

    add css logo

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import NavBar from './NavBar';
 
 const HomePage = () => {
   return (
     <Container>
-      <NavBar />
       <div className="home-container">
         <h1>Home page content here ...</h1>
       </div>
