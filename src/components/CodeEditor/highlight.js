32c43c6ac0b63c2bec186ce72b031860289d997d
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-04-18 18:59:02 +0530

    add text editor

---
+++ src/components/CodeEditor/highlight.js
@@ -0,0 +1,7 @@
+// highlight.js
+
+// assumption: package `highlight.js` is installed
+import highlight from 'highlight.js/lib/highlight';
+import 'highlight.js/styles/tomorrow-night-bright.css';
+
+window.hljs = highlight;
